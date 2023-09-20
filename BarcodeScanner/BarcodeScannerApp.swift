f65137c723fd6231894c0f9914216ebe4eaddd05
LuiArm <Luisarmendariz24@icloud.com>
2023-09-20 10:00:21 -0600

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by luis armendariz on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
